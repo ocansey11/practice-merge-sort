2b02b6deee0bd0a27601488f8067b0ce6fa762fa
kevin ocansey <kevinocansey11@gmail.com>
2023-01-15 20:11:33 +0000

    completed

--- merge-sort.js
+++ merge-sort.js
@@ -1,34 +1,64 @@
 // Merge Sort out-of-place
 // Do not modify the original array
 function mergeSort(arr) {
+  let splitIndex = 0
+  if(arr.length % 2 == 0){
+    splitIndex = arr.length/2
+  }
+  else{
+    splitIndex = (arr.length/2) - 0.5
+  }
 
   // Check if the input is length 1 or less
-    // If so, it's already sorted: return
+  // If so, it's already sorted: return
+    if(arr.length <= 1){
+      return arr
+    }
 
   // Divide the array in half
+  let arrA = arr.slice(0,splitIndex)
+  let arrB = arr.slice(splitIndex)
 
   // Recursively sort the left half
+  arr  = mergeSort(arrA)
   // Recursively sort the right half
+  arr = mergeSort(arrB)
 
   // Merge the halves together and return
+  return merge(arrA,arrB)
 
 }
 
 
 // Takes in two sorted arrays and returns them merged into one
 function merge(arrA, arrB) {
-
   // Create an empty return array
+  let results = []
 
   // Point to the first value of each array
+  let pointerA = arrA[0]
+  let pointerB = arrB[0]
+
   // While there are still values in each array...
+  while(arrA.length !== 0 && arrB.length!== 0){
     // Compare the first values of each array
     // Add the smaller value to the return array
     // Move the pointer to the next value in that array
-
+    if(pointerA < pointerB || pointerB == undefined){
+    results.push(pointerA)
+    arrA.splice(0,1)
+    pointerA = arrA[0]
+    }
+    if(pointerA > pointerB || pointerA == undefined){
+    results.push(pointerB)
+    arrB.splice(0,1)
+    pointerB = arrB[0]
+    }
+  }
   // Return the return array
-
+  // during the recursion, some split arrays either arrA or B have only 1 value and are returned from the stack due to the base case(before merging). we need to add them to the results along the way.
+  results =  results.concat(arrA).concat(arrB)
+  return results
 }
 
 module.exports = [merge, mergeSort];
-
